b66622ebc63b68f129730c00b8d0dbd9e6c90d7b
meilidao12 <330629898@qq.com>
2018-12-02 23:39:33 +0800

    删除空行

--- 01http模块.js
+++ 01http模块.js
@@ -18,19 +18,11 @@ var http=require('http');
      * */
 
 http.createServer(function(req,res){
-
-
- // 发送 HTTP 头部
-// HTTP 状态值: 200 : OK
-//设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf-8
-
+    // 发送 HTTP 头部
+    // HTTP 状态值: 200 : OK
+    //设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf-8
     res.writeHead(200,{"Content-Type":"text/html;charset='utf-8'"});
-
     res.write('你好 nodejs');
-
     res.write('我是第一个nodejs程序');
-
     res.end(); /*结束响应*/
-
-
 }).listen(8001);
