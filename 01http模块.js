62f97a639c3d88164a3d259b8d162980ee6d5417
meilidao12 <330629898@qq.com>
2018-12-03 11:38:03 +0800

    123

--- 01http模块.js
+++ 01http模块.js
@@ -10,19 +10,21 @@
 
 var http=require('http');
 
-//2.用http模块创建服务
+// //2.用http模块创建服务
 
-    /*
-     req获取url信息   （request）
-     res 浏览器返回响应信息 （response）
-    */
+//     /*
+//      req获取url信息   （request）
+//      res 浏览器返回响应信息 （response）
+//     */
 
 http.createServer(function(req,res){
     // 发送 HTTP 头部
     // HTTP 状态值: 200 : OK
     //设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf-8
-    res.writeHead(200,{"Content-Type":"text/html;charset='utf-8'"});
-    res.write('你好 nodejs');
-    res.write('我是第一个nodejs程序');
+    res.writeHead(200,{"Content-Type":"text/plain;charset='utf-8'"});
+    res.write('你好 nodejs/n');
+    res.write('我是第一个nodejs程序/n');
+    res.write('哈哈哈/n');
     res.end(); /*结束响应*/
 }).listen(7005);
+console.log("开始监听7005端口");
