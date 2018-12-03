a2c29af168fd14274480cdea07357bc0211b0b3c
meilidao12 <330629898@qq.com>
2018-12-03 16:35:17 +0800

    321

--- 01http模块.js
+++ 01http模块.js
@@ -9,22 +9,30 @@
 //1.引入http模块
 
 var http=require('http');
-
+var url = require('url');
 // //2.用http模块创建服务
 
 //     /*
 //      req获取url信息   （request）
 //      res 浏览器返回响应信息 （response）
 //     */
-
+var data;
 http.createServer(function(req,res){
     // 发送 HTTP 头部
     // HTTP 状态值: 200 : OK
     //设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf-8
     res.writeHead(200,{"Content-Type":"text/plain;charset='utf-8'"});
-    res.write('你好 nodejs/n');
-    res.write('我是第一个nodejs程序/n');
-    res.write('哈哈哈/n');
-    res.end(); /*结束响应*/
+    var param = url.parse(req.url,true).pathname;
+    var lsts = param.split('/');
+    console.log(lsts[4].toString());
+    if(lsts[4].toString() == "1234")
+    {
+        data = {"MaterialCode":"123","Message":"查询成功","SerialNumber":"1YHP000002A4402","Type":"S"}
+    }
+    else
+    {
+        data = {"MaterialCode":"","Message":"为查询到信息","SerialNumber":"1YHP000002A4402","Type":"E"}
+    }
+    res.end(JSON.stringify(data)); /*结束响应*/
 }).listen(7005);
 console.log("开始监听7005端口");
